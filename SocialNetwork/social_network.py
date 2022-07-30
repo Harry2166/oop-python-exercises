b6f359aae797aa1fdca549fd29241d04489e6cf5
Harry2166 <harry2166.hq@gmail.com>
2022-07-30 18:17:36 +0800

    Update the code

--- SocialNetwork/social_network.py
+++ SocialNetwork/social_network.py
@@ -46,6 +46,7 @@ class Person():
     self.name = name
     self.age = age
     self.birthday = birthday
+    self.friend_list = []
 
     if name == None:
       self.name = random.choice(name_list)
@@ -81,6 +82,9 @@ class SocialNetwork():
   def all_accounts(self):
     for account in self.accounts:
       print(account.name)
+
+  def send_friend_request(self, user1, user2): # -> there should be some sort of notification to user2 so that they can approve or disapprove of the friend request, will think of this next time!
+    return
   
   def __str__(self):
     return f"We are {self.name} and we currently have {len(self.accounts)} user(s)!"
